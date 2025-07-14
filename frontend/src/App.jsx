42b6b3550507e8670b6e0b76cf0f0e33a0ec4a1c
Vaishnavi Gupta <154550063+vaishnavi-agr@users.noreply.github.com>
2025-07-14 15:29:32 +0530

    Updated App.jsx : Includes the Dashboard Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import DashboardLayout from "./layouts/DashboardLayout";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Assistant from './pages/Assistant';
@@ -6,6 +7,11 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Dashboard from "./pages/Dashboard";
+import FamilyLog from "./components/Familylog";
+import AIHealthUpdate from "./pages/AIHealthUpdates";
+import DailyInfo from "./pages/DailyInfo";
+import ElderSupport from "./pages/ElderSupport";
 import Offline from './pages/Offline';
 
 import './index.css';
@@ -22,7 +28,13 @@ function App() {
             <Route path="/scanner" element={<Scanner />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            {/* <Route path="/dashboard" element={<Dashboard />} />  */}
+            <Route path="/dashboard" element={<DashboardLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="family-logs" element={<FamilyLog />} />
+            <Route path="ai-health-update" element={<AIHealthUpdate />} />
+            <Route path="daily-info" element={<DailyInfo />} />
+            <Route path="elder-support" element={<ElderSupport />} />
+            </Route>
             <Route path="/offline" element={<Offline />} /> 
           </Routes>
         </main>
